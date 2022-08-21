aacd5f56e0857bd607f834d92dd385472c8689da
crosstyan <crosstyan@outlook.com>
2022-08-21 05:53:47 +0000

    export C API as pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,15 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub mod C {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
 
 // https://stackoverflow.com/questions/24145823/how-do-i-convert-a-c-string-into-a-rust-string-and-back-via-ffi
 // https://doc.rust-lang.org/reference/items/extern-crates.htm
 
 pub mod agoraRTC {
+    use super::C::*;
     use super::*;
     use log::{error, info, warn};
     use num_derive::FromPrimitive;
